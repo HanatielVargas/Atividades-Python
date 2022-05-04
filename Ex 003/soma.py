2c9c7475efcd6960a5be14fee5823dbb35fa99d0
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-04 10:05:32 -0400

    Update soma.py

--- Ex 003/soma.py
+++ Ex 003/soma.py
@@ -1,2 +1,8 @@
-n1, n2 = int(input('Digite um número: ').strip()), int(input('Digite outro número: ').strip())
-print(f'A soma de {n1} e {n2} é igual a {n1 + n2}.')
+while True:
+    try:
+        n1, n2 = int(input('Digite um número: ').strip()), int(input('Digite outro número: ').strip())
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'A soma de {n1} e {n2} é igual a {n1 + n2}.')
+        break
