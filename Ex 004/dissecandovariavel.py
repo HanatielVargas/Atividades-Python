65c4ff8a870815eb881033665003843dc0b2a5cf
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-04 11:16:12 -0400

    Update dissecandovariavel.py

--- Ex 004/dissecandovariavel.py
+++ Ex 004/dissecandovariavel.py
@@ -1,2 +1,8 @@
-teste = input('Digite algo: ')
-print(f'O tipo primitivo desse valor é {type(teste)}', '\nSó tem espaços?', True if teste.isspace() else False, '\nÉ um número?', True if teste.isnumeric() else False, '\nÉ alfabético?', True if teste.isalpha() else False, '\nÉ alfanúmerico?', True if teste.isalnum() else False, '\nEstá em maiúsculas?', True if teste.isupper() else False, '\nEstá em minúsculas?', True if teste.islower() else False, '\nEstá capitalizada?', True if teste.istitle() else False)
+while True:
+    try:
+        teste = input('Digite algo: ')
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'O tipo primitivo desse valor é {type(teste)}', '\nSó tem espaços?', True if teste.isspace() else False, '\nÉ um número?', True if teste.isnumeric() else False, '\nÉ alfabético?', True if teste.isalpha() else False, '\nÉ alfanúmerico?', True if teste.isalnum() else False, '\nEstá em maiúsculas?', True if teste.isupper() else False, '\nEstá em minúsculas?', True if teste.islower() else False, '\nEstá capitalizada?', True if teste.istitle() else False)
+        break
