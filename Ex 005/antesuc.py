c77b6b960857095adb258815586e5faae38bf60d
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-05 09:47:56 -0400

    Corrigindo ortografia

--- Ex 005/antesuc.py
+++ Ex 005/antesuc.py
@@ -1,2 +1,2 @@
 num = int(input('Digite um número: '))
-print(f'Analisando o núemro {num}, seu antecessor é {num-1} e seu sucessor é {num+1}')
+print(f'Analisando o número {num}, seu antecessor é {num-1} e seu sucessor é {num+1}')
