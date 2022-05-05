b0d4dd89241b65af0f6611fdf924f75626ba64c0
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-05 15:23:12 -0400

    Update antesuc.py

--- Ex 005/antesuc.py
+++ Ex 005/antesuc.py
@@ -1,2 +1,8 @@
-num = int(input('Digite um número: '))
-print(f'Analisando o número {num}, seu antecessor é {num-1} e seu sucessor é {num+1}')
+while True:
+    try:
+        num = int(input('Digite um número: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'Analisando o número {num}, seu antecessor é {num-1} e seu sucessor é {num+1}')
+        break
