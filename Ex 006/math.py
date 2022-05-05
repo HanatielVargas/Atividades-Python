ae3b9a5ff41596ca7246a227fbe25096264a4f22
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-05 15:30:53 -0400

    Update math.py

--- Ex 006/math.py
+++ Ex 006/math.py
@@ -1,2 +1,8 @@
-num = int(input('Digite um número: '))
-print(f'O dobro de {num} é: {num*2} \nO triplo de {num} é: {num*3} \nA raiz quadrada de {num} é: {num**(1/2)}')
+while True:
+    try:
+        num = int(input('Digite um número: '))
+    except:
+        print('\033[1;31mOcooreu um erro!\033[m')
+    else:
+        print(f'O dobro de {num} é: {num*2} \nO triplo de {num} é: {num*3} \nA raiz quadrada de {num} é: {num**(1/2)}')
+        break
