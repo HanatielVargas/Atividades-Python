c08fc4be7a39fcce361b3735b5cc88fdefb09d27
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-06 14:53:36 -0400

    Update media.py

--- Ex 007/media.py
+++ Ex 007/media.py
@@ -1,3 +1,15 @@
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-print(f'A média de {n1} e {n2} é {(n1+n2)/2}')
+while True:
+    try:
+        n1 = float(input('Digite a primeira nota: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        while True:
+            try:
+                n2 = float(input('Digite a segunda nota: '))
+            except:
+                print('\033[1;31mOcorreu um erro!\033[m')
+            else:
+                print(f'A média de {n1} e {n2} é {(n1+n2)/2}')
+                break
+        break
