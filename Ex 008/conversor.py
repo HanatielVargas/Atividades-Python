c045e68b4ba3e4d250cbc14929bcb1fbdacdb3a3
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-06 14:02:46 -0400

    Update conversor.py

--- Ex 008/conversor.py
+++ Ex 008/conversor.py
@@ -1,2 +1,8 @@
-d = float(input('Uma distância em metros: '))
-print(f'A medida de {d} metros corresponde à: \n{d/1000}km \n{d/100}hm \n{d/10}dam \n{d*10}dm \n{d*100}cm \n{d*1000}mm')
+while True:
+    try:
+        d = float(input('Uma distância em metros: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'A medida de {d} metros corresponde à: \n{d/1000}km \n{d/100}hm \n{d/10}dam \n{d*10}dm \n{d*100}cm \n{d*1000}mm')
+        break
