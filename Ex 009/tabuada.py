6977ba6a2001a2befbc6921ed34b16061c9bfa51
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-10 20:30:09 -0400

    Update tabuada.py

--- Ex 009/tabuada.py
+++ Ex 009/tabuada.py
@@ -1,8 +1,11 @@
-num = int(input('Digite um número: '))
-
-print('-'*(len(f' {num} x 10 = {num*10} ')))
-
-for c in range(1, 11):
-    print(f' {num} x {c:>2} = {num*c}')
-
-print('-'*(len(f' {num} x 10 = {num*10} ')))
+while True:
+    try:
+        num = int(input('Digite um número: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print('-'*(len(f' {num} x 10 = {num*10} ')))
+        for c in range(1, 11):
+            print(f' {num} x {c:>2} = {num*c}')
+        print('-'*(len(f' {num} x 10 = {num*10} ')))
+        break
