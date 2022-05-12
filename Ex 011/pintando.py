12b80d64e2cb20d6ee1f44a9115a24f5bf1a836b
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-12 19:00:29 -0400

    Create pintando.py

---
+++ Ex 011/pintando.py
@@ -0,0 +1,4 @@
+larg = float(input('Largura: '))
+altu = float(input('Altura: '))
+print(f'Sua parede tem dimensões {larg}x{altu} e sua área é de {larg*altu}.')
+print(f'Para pintar essa parede será necessário {(larg*altu)/2}l de tinta.')
