e80b76530d629b8b80a96d1a2ed3875b71ed8420
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-12 19:40:28 -0400

    Update pintando.py

--- Ex 011/pintando.py
+++ Ex 011/pintando.py
@@ -1,4 +1,16 @@
-larg = float(input('Largura: '))
-altu = float(input('Altura: '))
-print(f'Sua parede tem dimensões {larg}x{altu} e sua área é de {larg*altu}.')
-print(f'Para pintar essa parede será necessário {(larg*altu)/2}l de tinta.')
+while True:
+    try:
+        larg = float(input('Largura: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        while True:
+            try:
+                altu = float(input('Altura: '))
+            except:
+                print('\033[1;31mOcorreu um erro!\033[m')
+            else:
+                print(f'Sua parede tem dimensões {larg}x{altu} e sua área é de {larg*altu}.')
+                print(f'Para pintar essa parede será necessário {(larg*altu)/2}l de tinta.')
+                break
+        break
