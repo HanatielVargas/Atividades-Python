e3aae950539bf4f1dde3dcfdd2b9e7a5a7eee37f
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-12 20:38:27 -0400

    Update desconto.py

--- Ex 012/desconto.py
+++ Ex 012/desconto.py
@@ -1,2 +1,8 @@
-preco = float(input('Qual o valor do produto? R$'))
-print(f'O produto que custava R${preco} com o desconto de 5% vai custar {preco*0.95:.2f}')
+while True:
+    try:
+        preco = float(input('Qual o valor do produto? R$'))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'O produto que custava R${preco} com o desconto de 5% vai custar {preco*0.95:.2f}')
+        break
