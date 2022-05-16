1bfc9a9c8c303d9e9f9102cd584900a6771f50b9
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-16 18:56:52 -0400

    Update conversor.py

--- Ex 014/conversor.py
+++ Ex 014/conversor.py
@@ -1,2 +1,8 @@
-temp = float(input('Informe a temperatura em °C: '))
-print(f'A temperatura de °C {temp} corresponde a °F {(temp*9/5)+32}.')
+while True:
+    try:
+        temp = float(input('Informe a temperatura em °C: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'A temperatura de °C {temp} corresponde a °F {(temp*9/5)+32}.')
+        break
