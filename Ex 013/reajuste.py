8ea7c9bf27de65f8ee67456778e92666c6dfa46f
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-16 18:55:22 -0400

    Update reajuste.py

--- Ex 013/reajuste.py
+++ Ex 013/reajuste.py
@@ -1,2 +1,8 @@
-valor = float(input('Digite o salário do funcionário: R$'))
-print(f'Um funcionário que ganhava R${valor} com o reajuste de 15% ganhará R${valor*1.15:.2f}.')
+while True:
+    try:
+        valor = float(input('Digite o salário do funcionário: R$'))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'Um funcionário que ganhava R${valor} com o reajuste de 15% ganhará R${valor*1.15:.2f}.')
+        break
