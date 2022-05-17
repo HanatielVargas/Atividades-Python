061e7df22d6e64f909dd73d835cbe747fa45e942
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-17 18:43:41 -0400

    Update num.py

--- Ex 016/num.py
+++ Ex 016/num.py
@@ -1,2 +1,8 @@
-valor = float(input('Digite um valor: '))
-print(f'O valor digitado foi {valor} e sua parte inteira é de {int(valor)}')
+while True:
+    try:
+        valor = float(input('Digite um valor: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'O valor digitado foi {valor} e sua parte inteira é de {int(valor)}')
+        break
