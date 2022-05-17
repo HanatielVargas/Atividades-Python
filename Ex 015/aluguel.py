a086ab0362e621204c810cce8c98fd4d7c886582
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-17 18:41:18 -0400

    Update aluguel.py

--- Ex 015/aluguel.py
+++ Ex 015/aluguel.py
@@ -1,4 +1,15 @@
-dias = int(input('Quantos dias foi utilizado? '))
-km = float(input('Quantos kms foram rodados? '))
-
-print(f'O total a pagar é de R${dias*60+km*0.15}')
+while True:
+    try:
+        dias = int(input('Quantos dias foi utilizado? '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        while True:
+            try:
+                km = float(input('Quantos kms foram rodados? '))
+            except:
+                print('\033[1;31mOcorreu um erro!\033[m')
+            else:
+                print(f'O total a pagar é de R${dias*60+km*0.15:.2f}')
+                break
+        break
