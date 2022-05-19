884fb604ddb9bca4e52de9171b8f10cb6a8cd057
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-18 21:22:28 -0400

    Update ang.py

--- Ex 018/ang.py
+++ Ex 018/ang.py
@@ -1,6 +1,14 @@
-from math import *
+try:
+    from math import *
+except:
+    print('\033[1;31Não foi possível importar a biblioteca "MATH"!\033[m')
+    quit()
 
-
-ang = float(input('Digite o ângulo que você deseja: '))
-print(f'O ângulo de {ang} tem o SENO de:\t\t{sin(radians(ang)):.2f} \nO ângulo de {ang} tem o COSSENO de:\t{cos(radians(ang)):.2f} \nO ângulo de {ang} tem a TANGENTE de:\t{tan(radians(ang)):.2f}')
-
+while True:
+    try:
+        ang = float(input('Digite o ângulo que você deseja: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'O ângulo de {ang} tem o SENO de:\t\t{sin(radians(ang)):.2f} \nO ângulo de {ang} tem o COSSENO de:\t{cos(radians(ang)):.2f} \nO ângulo de {ang} tem a TANGENTE de:\t{tan(radians(ang)):.2f}')
+        break
