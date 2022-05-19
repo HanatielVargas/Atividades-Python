33672bada3a6b754554097d6e1e74c8df0413cdf
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-18 21:15:46 -0400

    Create ang.py

---
+++ Ex 018/ang.py
@@ -0,0 +1,6 @@
+from math import *
+
+
+ang = float(input('Digite o ângulo que você deseja: '))
+print(f'O ângulo de {ang} tem o SENO de:\t\t{sin(radians(ang)):.2f} \nO ângulo de {ang} tem o COSSENO de:\t{cos(radians(ang)):.2f} \nO ângulo de {ang} tem a TANGENTE de:\t{tan(radians(ang)):.2f}')
+
