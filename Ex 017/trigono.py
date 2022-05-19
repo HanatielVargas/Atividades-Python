a924641255278bfce0a856fc0409f2a3e9c4f6c7
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-18 21:18:26 -0400

    Update trigono.py

--- Ex 017/trigono.py
+++ Ex 017/trigono.py
@@ -1,4 +1,15 @@
-co = float(input('Valor do Cateto Oposto: '))
-ca = float(input('Valor do Cateto Adjacente: '))
-
-print(f'A hipotenusa vai medir: {(ca**2+co**2)**0.5:.2f}')
+while True:
+    try:
+        co = float(input('Valor do Cateto Oposto: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        while True:
+            try:
+                ca = float(input('Valor do Cateto Adjacente: '))
+            except:
+                print('\033[1;31mOcorreu um erro!\033[m')
+            else:
+                print(f'A hipotenusa vai medir: {(ca**2+co**2)**0.5:.2f}')
+                break
+        break
