5f59a47c7cea7feb043dd0118355dcb39dc70f43
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-20 19:00:05 -0400

    Update shuf.py

--- Ex 020/shuf.py
+++ Ex 020/shuf.py
@@ -1,5 +1,21 @@
 from random import shuffle
 
-nomes = list((input('Primeiro aluno: ').title(), input('Segundo aluno: ').title(), input('Terceiro aluno: ').title(), input('Quarto aluno: ').title()))
+nomes = list()
+
+for c in range(4):
+    while True:
+        try:
+            nome = str(input(f'{c+1}° Aluno: ')).title().strip()
+            if nome == '' or not nome.isalpha():
+                raise KeyboardInterrupt
+        except:
+            print('\033[1;31mOcorreu um erro!\033[m')
+        else:
+            nomes.append(nome)
+            break
+
 shuffle(nomes)
-print(f'A ordem de apresentação será: \n{nomes}')
+print('\nA ordem das apresentações será:')
+
+for i, c in enumerate(nomes):
+    print(f'{i+1}°  {c}')
