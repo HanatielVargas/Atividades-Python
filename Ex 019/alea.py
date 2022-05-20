b2b7b7bb42f4c8574eae2060a227bda915dd1909
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-20 18:57:35 -0400

    Update alea.py

--- Ex 019/alea.py
+++ Ex 019/alea.py
@@ -1,5 +1,18 @@
 from random import shuffle
 
-nomes = list((input('Primeiro aluno: ').title(), input('Segundo aluno: ').title(), input('Terceiro aluno: ').title(), input('Quarto aluno: ').title()))
+nomes = list()
+
+for c in range(4):
+    while True:
+        try:
+            nome = str(input(f'{c+1}° Aluno: ')).title().strip()
+            if nome == '' or not nome.isalpha():
+                raise KeyboardInterrupt
+        except:
+            print('\033[1;31mOcorreu um erro!\033[m')
+        else:
+            nomes.append(nome)
+            break
+
 shuffle(nomes)
 print(f'O aluno sorteado foi {nomes[0]}')
