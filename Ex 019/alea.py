6f83b5d045463bd18da390428e1ec887d80844c7
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-20 18:39:07 -0400

    Update alea.py

--- Ex 019/alea.py
+++ Ex 019/alea.py
@@ -1,5 +1,5 @@
 from random import shuffle
 
-nomes = list((input('Primeiro aluno: ').title(), input('Segundo aluno: ').title(), input('Terceiro aluno: ').title, input('Quarto aluno: ').title()))
+nomes = list((input('Primeiro aluno: ').title(), input('Segundo aluno: ').title(), input('Terceiro aluno: ').title(), input('Quarto aluno: ').title()))
 shuffle(nomes)
 print(f'O aluno sorteado foi {nomes[0]}')
