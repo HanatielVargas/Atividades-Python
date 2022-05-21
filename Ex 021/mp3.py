960605c0d86466d02b7fa963099de9aeb5c6ab70
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-21 13:25:35 -0400

    Update mp3.py

--- Ex 021/mp3.py
+++ Ex 021/mp3.py
@@ -1,7 +1,13 @@
-from pygame import mixer
+try:
+    from pygame import mixer
 
-mixer.init()
-mixer.music.load('Ex 021/musica.mp3')
-mixer.music.play()
-
-a = input('Digite enter para sair')
+    mixer.init()
+    mixer.music.load('Ex 021/musica.mp3')
+    mixer.music.play()
+except:
+    print('\033[1;31mOcorreu um erro!\033[m')
+else:
+    try:
+        a = input('Digite enter para sair ')
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
