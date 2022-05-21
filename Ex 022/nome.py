d1e0247ed2d5c30879b84b18dbfa6b9ec93b7229
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-21 13:18:00 -0400

    Update nome.py

--- Ex 022/nome.py
+++ Ex 022/nome.py
@@ -1,7 +1,12 @@
-nome = input('Digite seu nome completo: ').strip()
-
-print(f'''Analisando seu nome...
-Seu nome em maiúsculas é {nome.upper()}
-Seu nome em minúsculas é {nome.lower()}
-Seu nome tem ao todo {len(nome)-(len(nome.split())-1)} letras
-Seu primeiro nome é {nome.split()[0]} e ele tem {len(nome.split()[0])} letras''')
+while True:
+    try:
+        nome = input('Digite seu nome completo: ').strip()
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'''Analisando seu nome...
+        Seu nome em maiúsculas é {nome.upper()}
+        Seu nome em minúsculas é {nome.lower()}
+        Seu nome tem ao todo {len(nome)-(len(nome.split())-1)} letras
+        Seu primeiro nome é {nome.split()[0]} e ele tem {len(nome.split()[0])} letras''')
+        break
