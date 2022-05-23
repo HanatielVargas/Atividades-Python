f93fd4923ac83bc5690f27823caf302136aa7a89
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-23 19:02:27 -0400

    Update santo.py

--- Ex 024/santo.py
+++ Ex 024/santo.py
@@ -1,3 +1,8 @@
-cidade = input('Digite o nome da cidade: ').strip()
-
-print(True if cidade.split()[0].lower() == 'santo' else False)
+while True:
+    try:
+        cidade = input('Digite o nome da cidade: ').strip()
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(True if cidade.split()[0].lower() == 'santo' else False)
+        break
