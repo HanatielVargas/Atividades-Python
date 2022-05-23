b0bd2f2a5e5a8f0269b44eebbf82345a5cf2ef9a
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-23 19:00:34 -0400

    Update sep.py

--- Ex 023/sep.py
+++ Ex 023/sep.py
@@ -1,8 +1,13 @@
-num = input('Digite um número: ')
-
-print(f'''Analisando o número {num}
+while True:
+    try:
+        num = input('Digite um número: ')
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'''Analisando o número {num}
 Unidade: {num[0] if len(num) > 0 else 0}
 Dezena: {num[1] if len(num) > 1 else 0}
 Centenas: {num[2] if len(num) > 2 else 0}
 Milhar: {num[3] if len(num) > 3 else 0}
 ''')
+        break
