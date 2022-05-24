02f4cda127dd1cda83de84ef4f627c42cd4fde55
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-24 19:09:41 -0400

    Update a.py

--- Ex 026/a.py
+++ Ex 026/a.py
@@ -1,7 +1,14 @@
-frase = ' '.join(input('Digite uma frase: ').strip().lower().split())
-
-print(f'''{frase.title()}
+while True:
+    try:
+        frase = ' '.join(input('Digite uma frase: ').strip().lower().split())
+        if len(frase) == 0:
+            raise KeyError
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'''{frase.title()}
 A letra A aparece {frase.count('a')} vezes na frase
-A primeira letra A aparece na posição {frase.find('a')+1}
-A últims letra A aparece na posição {frase.rfind('a')+1}
+A primeira letra A aparece na posição {frase.find('a')+1 if frase.find('a') != -1 else 'xx'}
+A última letra A aparece na posição {frase.rfind('a')+1 if frase.rfind('a') != -1 else 'xx'}
 ''')
+        break
