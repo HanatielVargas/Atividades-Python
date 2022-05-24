f59c06f2d2a89b350da8101d0db61b1558e26ce1
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-24 19:01:08 -0400

    Update search.py

--- Ex 025/search.py
+++ Ex 025/search.py
@@ -1,1 +1,10 @@
-print('Seu nome tem Silva?', True if 'Silva' in input('Digite seu nome completo: ').strip().title() else False)
+while True:
+    try:
+        nome = ' '.join(input('Digite seu nome completo: ').title().split())
+        if len(nome) == 0 or not nome.isalpha():
+            raise KeyError
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print('Seu nome tem Silva?', True if 'Silva' in  nome else False)
+        break
