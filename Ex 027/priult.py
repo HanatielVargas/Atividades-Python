9324d803472f44a0b729047645f927c028d0a42f
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-25 21:14:51 -0400

    Update priult.py

--- Ex 027/priult.py
+++ Ex 027/priult.py
@@ -1,6 +1,13 @@
-nome = ' '.join(input('Digite seu nome completo: ').strip().title().split())
-
-print(f'''Prazer em te conhecer!
+while True:
+    try:
+        nome = ' '.join(input('Digite seu nome completo: ').strip().title().split())
+        if nome == '' or not nome.isalpha():
+            raise KeyError
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'''Prazer em te conhecer!
 Seu primeiro nome é {nome.split()[0]}
 Seu último nome é {nome.split()[-1]}
 ''')
+        break
