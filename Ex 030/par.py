ead4819d66fcd3813a441f944da5fa02cd8a09cf
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-27 21:43:54 -0400

    Update par.py

--- Ex 030/par.py
+++ Ex 030/par.py
@@ -1,3 +1,8 @@
-num = int(input('Digite um número: '))
-
-print(f'O número {num} é', 'ÍMPAR' if num%2 != 0 else 'PAR')
+while True:
+    try:
+        num = int(input('Digite um número: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print(f'O número {num} é', 'ÍMPAR' if num%2 != 0 else 'PAR')
+        break
