a3813c8591c088ec092b8d4109212e1c62b0ca7c
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-27 21:46:18 -0400

    Update multa.py

--- Ex 029/multa.py
+++ Ex 029/multa.py
@@ -1,3 +1,8 @@
-vel = float(input('Digite sua velocidade: '))
-
-print('Tenha um bom dia! Dirija com segurança' if vel <= 80 else f'\033[1;31mMULTADO! Você excedeu o limite permitido que é de 80km/h \nVocê deve pagar uma multa de R${(vel-80)*7:.2f}!\033[m\nTenha um bom dia! Dirija com segurança')
+while True:
+    try:
+        vel = float(input('Digite sua velocidade: '))
+    except:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    else:
+        print('Tenha um bom dia! Dirija com segurança' if vel <= 80 else f'\033[1;31mMULTADO! Você excedeu o limite permitido que é de 80km/h \nVocê deve pagar uma multa de R${(vel-80)*7:.2f}!\033[m\nTenha um bom dia! Dirija com segurança')
+        break
