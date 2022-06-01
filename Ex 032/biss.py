0a219865604f9b9c0e1c4eb47e956635a169d7d0
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-31 21:21:29 -0400

    Update biss.py

--- Ex 032/biss.py
+++ Ex 032/biss.py
@@ -1,7 +1,15 @@
-ano = input('Digite um ano: ').strip()
-intano = int(ano)
-
-if intano % 4 == 0 and intano % 100 != 0 or intano % 400 == 0:
-    print(True)
-else:
-    print(False)
+while True:
+    try:
+        ano = input('Digite um ano: ').strip()
+        intano = int(ano)
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        break
+    else:
+        if intano % 4 == 0 and intano % 100 != 0 or intano % 400 == 0:
+            print(True)
+        else:
+            print(False)
+        break
