d471b7cd77c2ec4d375d1ef2117869c2f90bcff3
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-31 20:57:08 -0400

    Update dist.py

--- Ex 031/dist.py
+++ Ex 031/dist.py
@@ -1,8 +1,16 @@
-dist = float(input('Qual a distância da viagem em km? '))
-
-if dist <= 200:
-    valorpkm = 0.5
-else: 
-    valorpkm = 0.45
-print(f'Você está prestes a começar um viagem de {dist:.2f}km')
-print(f'E o preço da passagem custará R${dist*valorpkm}')
+while True:
+    try:
+        dist = float(input('Qual a distância da viagem em km? '))
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        break
+    else:
+        if dist <= 200:
+            valorpkm = 0.5
+        else: 
+            valorpkm = 0.45
+        print(f'Você está prestes a começar um viagem de {dist:.2f}km')
+        print(f'E o preço da passagem custará R${dist*valorpkm}')
+        break
