10f556a15fe9d550fbf1caf1cd92ed15638b0e46
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-07 22:07:44 -0400

    Update maxmin.py

--- Ex 033/maxmin.py
+++ Ex 033/maxmin.py
@@ -1,2 +1,38 @@
-nums = (int(input('Primeiro valor: ')), int(input('Segundo valor: ')), int(input('Terceiro valor: ')))
-print(max(nums), min(nums))
+nums = list()
+
+while True:
+    try:
+        num1 = int(input('Primeiro valor: '))
+        nums.append(num1)
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('\033[1;31mUsuário não quis continuar!\033[m')
+        quit()
+    else:
+        while True:
+            try:
+                num2 = int(input('Segundo valor: '))
+                nums.append(num2)
+            except Exception:
+                print('\033[1;31mOcorreu um erro!\033[m')
+            except KeyboardInterrupt:
+                print('\033[1;31mUsuário não quis continuar!\033[m')
+                quit()
+            else:
+                while True:
+                    try:
+                        num3 = int(input('Terceiro valor: '))
+                        nums.append(num3)
+                    except Exception:
+                        print('\033[1;31mOcorreu um erro!\033[m')
+                    except KeyboardInterrupt:
+                        print('\033[1;31mUsuário não quis continuar!\033[m')
+                        quit()
+                    else:
+                        break
+                break
+        break
+
+print(f'O menor valor digitado foi {min(nums)}')
+print(f'O maior número digitado foi {max(nums)}')
