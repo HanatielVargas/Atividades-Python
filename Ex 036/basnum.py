135e79473a26536c14a59cbb4979c8a87d46dd63
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-11 19:22:53 -0400

    Update basnum.py

--- Ex 036/basnum.py
+++ Ex 036/basnum.py
@@ -1,16 +1,35 @@
-num = int(input('Digite um número inteiro: '))
+while True:
+    try:
+        num = int(input('Digite um número inteiro: '))
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        exit()
+    else:
+        break
 
-print('''Escolha uma das bases para conversão:
+while True:
+    try:
+        opcao = int(input('''Escolha uma das bases para conversão:
+[0] Sair
 [1] Binário
 [2] Octal
-[3] Hexadecimal''')
-opcao = int(input('Sua opção: '))
+[3] Hexadecimal
+Sua opção: '''))
+        if opcao not in [1, 2, 3, 0]:
+            raise IndexError
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        exit()
+    else:
+        break
 
 if opcao == 1:
     print(f'{num} convertido para binário é: {bin(num)[2:]}')
 elif opcao == 2:
     print(f'{num} convertido para octal é: {oct(num)[2:]}')
 elif opcao == 3:
-    print(f'{num} convertido para hexadecimal é: {hex(num)[2:]}')
-else:
-    print(f'"{opcao}" não faz parte das opções disponíveis')
+    print(f'{num} convertido para hexadecimal é: {hex(num).upper()[2:]}')
