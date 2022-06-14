99800b82f437b6ee500d0e929a5004139a097fdf
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-13 20:14:32 -0400

    Update compnum.py

--- Ex 038/compnum.py
+++ Ex 038/compnum.py
@@ -1,5 +1,24 @@
-num1 = int(input('Digite o primeiro número: '))
-num2 = int(input('Digite o segundo número: '))
+while True:
+    try:
+        num1 = int(input('Digite o primeiro número: '))
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar!')
+        quit()
+    else:
+        break
+
+while True:
+    try:
+        num2 = int(input('Digite o segundo número: '))
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar!')
+        quit()
+    else:
+        break
 
 if num1 > num2:
     print('O primeiro número é maior')
