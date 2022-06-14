f8a320992e2f071b455f2826d3e72ec0bb243819
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-14 18:17:52 -0400

    Update media.py

--- Ex 040/media.py
+++ Ex 040/media.py
@@ -1,5 +1,29 @@
-nota1 = float(input('Primeira nota: '))
-nota2 = float(input('Segunda nota: '))
+while True:
+    try:
+        nota1 = float(input('Primeira nota: '))
+        if nota1 > 10 or nota1 < 0:
+            raise Exception
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        quit()
+    else:
+        break
+
+while True:
+    try:
+        nota2 = float(input('Segunda nota: '))
+        if nota2 > 10 or nota2 < 0:
+            raise Exception
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        quit()
+    else:
+        break
+
 media = (nota1+nota2)/2
 print(f'Tirando {nota1} e {nota2}, a média do aluno é {media}')
 print('O aluno está ', end='')
