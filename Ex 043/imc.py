ee79175860bc0176954b794def6bbaa9365ff62f
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-16 16:27:21 -0400

    Update imc.py

--- Ex 043/imc.py
+++ Ex 043/imc.py
@@ -1,18 +1,38 @@
-kg = float(input('Sua massa corporal: (KG) '))
-tam = float(input('Sua Altura: (M) '))
+while True:
+    try:
+        kg = float(input('Sua massa corporal: (KG) '))
+        if kg <= 0:
+            raise ZeroDivisionError
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        quit()
+    else:
+        break
 
-imc = kg/tam**2
+while True:
+    try:
+        tam = float(input('Sua Altura: (M) '))
+        imc = kg/tam**2
+    except Exception:
+        print('\033[1;31mOcorreu um erro!\033[m')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        quit()
+    else:
+        break
 
 print(f'O IMC dessa pessoa é {imc:.2f}')
-print('Essa pessoa está em estado de ', end='')
+print('Essa pessoa está ', end='')
 
 if imc < 18.5:
     print('Abaixo do peso')
 elif imc < 25:
-    print('Peso ideal')
+    print('com o Peso ideal')
 elif imc < 30:
-    print('Sobrepeso')
+    print('em Sobrepeso')
 elif imc < 40:
-    print('Obesidade')
+    print('com Obesidade')
 else:
-    print('Obesidade Mórbida')
+    print('com Obesidade Mórbida')
