d12e157dba8f3bf3c0f652289451590b86ed60d1
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-15 22:19:35 -0400

    Update triang2.py

--- Ex 042/triang2.py
+++ Ex 042/triang2.py
@@ -1,13 +1,48 @@
-reta1 = float(input('Primeira reta: '))
-reta2 = float(input('Segunda reta: '))
-reta3 = float(input('Terceira reta: '))
+while True:
 
-if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
+    while True:
+        try:
+            reta1 = float(input('Primeira reta: '))
+        except Exception:
+            print('\033[1;31mOcorreu um erro!\033[m')
+        except KeyboardInterrupt:
+            print('Usuário não quer continuar')
+            quit()
+        else:
+            break
 
-    if reta1 == reta2 == reta3:
-        print('Equilátero')
-    elif reta1 == reta2 or reta1 == reta3 or reta2 == reta3:
-        print('Isósceles')
-    elif reta1 != reta2 != reta3:
-        print('Escaleno')
- 
+    while True:
+        try:
+            reta2 = float(input('Segunda reta: '))
+        except Exception:
+            print('\033[1;31mOcorreu um erro!\033[m')
+        except KeyboardInterrupt:
+            print('Usuário não quer continuar')
+            quit()
+        else:
+            break
+
+    while True:
+        try:
+            reta3 = float(input('Terceira reta: '))
+        except Exception:
+            print('\033[1;31mOcorreu um erro!\033[m')
+        except KeyboardInterrupt:
+            print('Usuário não quer continuar')
+            quit()
+        else:
+            break
+
+    if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
+
+        if reta1 == reta2 == reta3:
+            print('Equilátero')
+        elif reta1 == reta2 or reta1 == reta3 or reta2 == reta3:
+            print('Isósceles')
+        elif reta1 != reta2 != reta3:
+            print('Escaleno')
+        break
+    
+    else:
+        print('Digite valores que formam um triângulo. \nPara sair aperte Ctrl+C a qualquer momento.')
+    
