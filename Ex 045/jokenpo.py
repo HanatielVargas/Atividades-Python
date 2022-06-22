bac4ab32ee223a16a7f1d5c25f5cce129c1a0d00
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-21 21:57:04 -0400

    Update jokenpo.py

--- Ex 045/jokenpo.py
+++ Ex 045/jokenpo.py
@@ -1,13 +1,37 @@
-from random import choice
-from time import sleep
+try:
+    from random import choice
+except ImportError:
+    print('Erro ao importar módulo necessário.')
+    quit()
+
+dormir = True
+
+try:
+    from time import sleep
+except:
+    dormir = False
 
 opc = ('Pedra', 'Papel', 'Tesoura')
 
-print('''Suas opções:
+while True:
+    try:
+        pes = int(input('''Suas opções:
 [1] Pedra
 [2] Papel
-[3] Tesoura''')
-pes = int(input('Qual sua escolha? '))
+[3] Tesoura
+Qual sua escolha? '''))
+        if pes < 1 or pes > 3:
+            raise IndexError
+    except IndexError:
+        print('Digite um número válido')
+    except Exception:
+        print('Ocorreu um erro')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        quit()
+    else:
+        break
+
 com = choice(opc)
 
 if pes == 1:
@@ -24,11 +48,11 @@ elif pes == 'Pedra' and com == 'Tesoura' or pes == 'Papel' and com == 'Pedra' or
 else:
     venc = 'Computador'
 
-sleep(0.5)
+sleep(0.5) if dormir == True else print(end='')
 print('JO')
-sleep(0.5)
+sleep(0.5) if dormir == True else print(end='')
 print('KEN')
-sleep(0.5)
+sleep(0.5) if dormir == True else print(end='')
 print('PO!')
 
 print(('-='*15)[:-1])
