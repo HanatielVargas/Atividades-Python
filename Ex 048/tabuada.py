1974a72f2d2681c097d5d619bfdcd3f7db84413a
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-27 20:03:22 -0400

    Update tabuada.py

--- Ex 048/tabuada.py
+++ Ex 048/tabuada.py
@@ -1,4 +1,12 @@
-num = int(input('Digite um número para ver sua tabuada: '))
-
-for c in range(10):
-    print(f'{num} X {c+1:>2} = {num*(c+1)}')
+while True:
+    try:
+        num = int(input('Digite um número para ver sua tabuada: '))
+    except Exception:
+        print('Ocorreu um Erro')
+    except KeyboardInterrupt:
+        print('Usuário não quer mais continuar')
+        quit()
+    else:
+        for c in range(10):
+            print(f'{num} X {c+1:>2} = {num*(c+1)}')
+        break
