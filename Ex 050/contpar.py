e0f05a269952dc33ad82cc8c06265616fa47b9fd
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-28 20:52:04 -0400

    Update contpar.py

--- Ex 050/contpar.py
+++ Ex 050/contpar.py
@@ -1,7 +1,16 @@
 pares = list()
 
 for c in range(6):
-    num = int(input(f'Digite o {c+1}º número: '))
+    while True:
+        try:
+            num = int(input(f'Digite o {c+1}º número: '))
+        except Exception:
+            print('\033[1;31mOcorreu um erro!\033[m')
+        except KeyboardInterrupt:
+            print('Usuário não quer continuar')
+            quit()
+        else:
+            break
     if num % 2 == 0:
         pares.append(num)
 
