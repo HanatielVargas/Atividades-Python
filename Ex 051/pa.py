1d6c50f27f644ad452b5ab70b90532b4a25e3b0c
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-07-18 20:37:23 -0400

    Update pa.py

--- Ex 051/pa.py
+++ Ex 051/pa.py
@@ -2,8 +2,28 @@
 Progreção Aritimétca
 '''
 
-ini = float(input('Início da PA: '))
-raz = float(input('Razão da PA: '))
+while True:
+    try:
+        ini = float(input('Início da PA: '))
+    except Exception:
+        print('Ocorreu um erro')
+    except KeyboardInterrupt:
+        print('Programa Interrompido')
+        exit()
+    else:
+        break
+
+while True:
+    try:
+        raz = float(input('Razão da PA: '))
+    except Exception:
+        print('Ocorreu um erro')
+    except KeyboardInterrupt:
+        print('Programa Interrompido')
+        exit()
+    else:
+        break
+
 atu = ini
 
 for c in range(10):
