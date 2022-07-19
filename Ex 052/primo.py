fa0bb43c2dcbbc8b09ea3e3803d59a47a337fac3
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-07-18 20:34:44 -0400

    Update primo.py

--- Ex 052/primo.py
+++ Ex 052/primo.py
@@ -2,17 +2,24 @@
 Número primo
 '''
 
-num = int(input('Digite um número: '))
+while True:
+    try:
+        num = int(input('Digite um número: '))
+    except Exception:
+        print('Ocorreu um erro')
+    except KeyboardInterrupt:
+        print('Programa interrompido')
+        exit()
+    else:
+        break
 
 soma = 0 
 
 for c in range(1, num+1, 1):
     if num % c == 0:
-        print(c)
         soma += 1
 
 if soma == 2:
     print(f'O número {num} é primo')
 else:
     print(f'O número {num} não é primo')
-    print(soma)
