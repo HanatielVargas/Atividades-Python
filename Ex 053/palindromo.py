5dcb363e4998f736d73637dff713b9557b7ae334
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-07-21 21:42:03 -0400

    Update palindromo.py

--- Ex 053/palindromo.py
+++ Ex 053/palindromo.py
@@ -1,4 +1,17 @@
-word = str(input('Digite uma palavra: ')).strip().lower()
+while True:
+    try:
+        word = ''.join(str(input('Digite uma frase: ')).strip().lower().split())
+        if word == '':
+            raise Exception
+    except Exception:
+        print('Ocorreu um erro!')
+    except KeyboardInterrupt:
+        print('Usuário não quer continuar')
+        exit()
+    else:
+        break
+
+print(f'O inverso de {word} é {word[-1::-1]}')
 
 if word == word[-1::-1]:
     print('É um Palíndromo')
