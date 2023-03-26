7293fa3fea6528ca5db7940f375c1c5463ec18fc
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-03-26 16:45:18 -0400

    Update advinhacaov2.py

--- Ex 058/advinhacaov2.py
+++ Ex 058/advinhacaov2.py
@@ -7,18 +7,34 @@ from random import randint
 num = randint(0, 10)
 qtde = 0
 tent = int
-print(num)
+numeros = list()
 
 print('Acabei de pensar num número de 0 a 10.')
 print('Será que você consegue advinhar qual foi?')
 
 while tent != num:
     qtde += 1
-    tent = int(input('Qual é o seu palpite? '))
+    while True:
+        try:
+            tent = int(input('Qual é o seu palpite? '))
+            if tent in numeros or tent < 0 or tent > 10:
+                raise SyntaxError
+        except KeyboardInterrupt:
+            print('O usuário não quer continuar!\nEncerrando.')
+            exit()
+        except SyntaxError:
+            print('Digite um número válido não digitado antes.')
+        except:
+            print('\033[1;31mOcorreu um problema.\033[m Escreva certo!')
+        else:
+            break
+        finally:
+            if tent not in numeros:
+                numeros.append(tent)
 
     if tent < num:
         print('Mais... Tente mais uma vez.')
     elif tent > num:
         print('Menos... Tente mais uma vez.')
 
-print(f'Acertou com {qtde} tentativas. Parabéns!')
+print(f'Acertou com {qtde} tentativa(s). Parabéns!')
