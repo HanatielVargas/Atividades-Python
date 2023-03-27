ebd9a61f9b5566d62eb50e32f9ecd26a5cd8471e
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-03-27 11:08:54 -0400

    Update menu.py

--- Ex 059/menu.py
+++ Ex 059/menu.py
@@ -2,17 +2,35 @@
 Criando um menu básico repetitível
 '''
 
-num1 = int(input('Primeiro Valor: '))
-num2 = int(input('Segundo Valor: '))
+inv = '\033[1;31mValor digitado é inválido.\033[m'
+num1 = int
+num2 = int
+menu = 4
+
+
+def saindo(s=True):
+    if s == True:
+        print('\nUsuário não quer continuar!')
+    print('Encerrando...')
+    exit()
+
 
 while True:
-    menu = int(input('''[1] Somar
+    if num1 != int:
+        while True:
+            try:
+                menu = int(input('''[1] Somar
 [2] Multiplicar
 [3] Maior
 [4] Novos Números
 [5] Sair do Programa
 >>>> Qual é a sua opção? '''))
-    
+            except KeyboardInterrupt:
+                saindo()
+            except:
+                print(inv)
+            else:
+                break
     if menu == 1:
         print(f'A soma de {num1} e {num2} é igual a {num1+num2}.') # type: ignore
     elif menu == 2:
@@ -25,13 +43,26 @@ while True:
         else:
             print(f'Os números {num1} e {num2} são iguais.')
     elif menu == 4:
-        num1 = int(input('Primeiro Valor: '))
-        num2 = int(input('Segundo Valor: '))
+            while True:
+                try:
+                    num1 = int(input('Primeiro Valor: '))
+                except KeyboardInterrupt:
+                    saindo()
+                except:
+                    print(inv)
+                else:
+                    break
+            while True:
+                try:
+                    num2 = int(input('Segundo Valor: '))
+                except KeyboardInterrupt:
+                    saindo()
+                except:
+                    print(inv)
+                else:
+                    break
     elif menu == 5:
-        print('Encerrando...')
-        exit()
+        saindo(s=False)
     else:
-        print('\033[1;31mValor inválido.\033[m')
-
+        print(inv)
     print('=-='*10)
-        
