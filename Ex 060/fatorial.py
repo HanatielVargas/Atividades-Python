efe5607bbc841121b717ee5133cbe1408ab840be
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-03-27 11:58:42 -0400

    Update fatorial.py

--- Ex 060/fatorial.py
+++ Ex 060/fatorial.py
@@ -2,13 +2,22 @@
 calculando fatorial
 '''
 
-num = int(input('Digite um número para calcular seu fatorial: '))
+while True:
+    try:
+        num = int(input('Digite um número para calcular seu fatorial: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...')
+        exit()
+    except:
+        print('\033[1;31mValor digitado é inválido.\033[m')
+    else:
+        break
 
 fat = 1
-cam = ''
+cam = '= '
 
 for c in range(num, 0, -1):
     fat *= c
     cam += f'{c} X '
 
-print(f'Calculando {num}! = {cam[0:-3]} = {fat}\n')
+print(f'Calculando {num}! {cam[0:-2]}= {fat}\n')
