a2a11572d7ddd9369cd100fb4bcf2aada6e0c7a2
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-03-28 12:20:20 -0400

    Update pav3.py

--- Ex 062/pav3.py
+++ Ex 062/pav3.py
@@ -11,14 +11,46 @@ def gerarpa(limite=10, cont=0):
 
 print('Gerador de PA\n' + '=-'*10)
 
-pri = int(input('Primeiro termo: '))
-raz = int(input('Razão da PA: '))
+while True:
+    try:
+        pri = int(input('Primeiro termo: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar.\nEncerrando...')
+        exit()
+    except:
+        print('\033[1;31mDigite algo válido.\033[m')
+    else:
+        break
+
+while True:
+    try:
+        raz = int(input('Razão da PA: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar.\nEncerrando...')
+        exit()
+    except:
+        print('\033[1;31mDigite algo válido.\033[m')
+    else:
+        break
 
 cont = gerarpa()
 
 while True:
-    mais = int(input('Quantos você quer mostrar a mais? '))
+    while True:
+        try:
+            mais = int(input('Quantos você quer mostrar a mais? '))
+            if mais < 0:
+                raise ValueError
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar.\nEncerrando...')
+            exit()
+        except:
+            print('\033[1;31mDigite algo válido.\033[m')
+        else:
+            break
+
     if mais == 0:
         print(f'Progressão finalizada com {cont} termos mostrados.')
         break
+
     cont = gerarpa(limite=(cont+mais), cont=cont)
