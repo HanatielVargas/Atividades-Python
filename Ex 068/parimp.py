b890ca6adeac90e3f15b43d3106f214460542b0d
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-01 15:02:47 -0400

    Update parimp.py

--- Ex 068/parimp.py
+++ Ex 068/parimp.py
@@ -11,21 +11,43 @@ print('   VAMOS JOGAR PAR OU ÍMPAR')
 while True:
     print('=-'*15)
 
-    val = int(input(' Diga um valor: '))
-    pi = str(input(' Par ou Ímpar? [P/I] ')).upper()[0]
+    while True:
+        try:
+            val = int(input(' Diga um valor: '))
+            if val < 0 or val > 10:
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\n Usuário não quer mais continuar')
+            quit()
+        except:
+            print('\033[1;31m Ocorreu um Erro\033[m')
+        else:
+            break
+
+    while True:
+        try:
+            tipo = str(input(' Par ou Ímpar? [P/I] ')).strip().upper()[0]
+            if tipo not in 'PI':
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\n Usuário não quer mais continuar')
+            quit()
+        except:
+            print('\033[1;31m Ocorreu um Erro\033[m')
+        else:
+            break
 
     comp = randint(0, 10)
     comp_win = False
-
     soma = comp+val
 
     if soma % 2 == 0:
         parimp = "Par"
-        if pi != 'P':
+        if tipo != 'P':
             comp_win = True
     else:
         parimp = 'Ímpar'
-        if pi != 'I':
+        if tipo != 'I':
             comp_win = True
 
     print('-'*30)
@@ -33,13 +55,12 @@ while True:
     print('-'*30)
 
     if comp_win == True:
-        print(' Você Perdeu!')
+        print('\033[1;31m Você Perdeu!\033[m')
         break
-    else:
-        print(' Você Venceu!') 
-        cont += 1
-
+    
+    print('\033[1;32m Você Venceu!\033[m') 
     print(' Vamos jogar novamente...')
+    cont += 1
 
 print('=-'*15)
-print(f' GAME OVER!\n Você venceu {cont} vez(es).\n')
+print(f' \033[1;31mGAME OVER!\033[m\n Você venceu {cont} vez(es).\n')
