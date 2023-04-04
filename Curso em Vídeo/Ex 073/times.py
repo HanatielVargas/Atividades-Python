6c009e5233c77e05ac9d3dad3a19a9102ab57b6e
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-04 14:17:28 -0400

    Create times.py

---
+++ Curso em Vídeo/Ex 073/times.py
@@ -0,0 +1,13 @@
+'''
+Aprendendo tuplas com o brasileirão
+'''
+
+times22 = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athletico-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo', 'Santos', 'Goiás', 'Bragantino', 'Coritiba', 'Cuiabá', 'Ceará SC', 'Atlético-GO', 'Avaí', 'Juventude')
+
+LINHA = '-='*30
+
+print(f'{LINHA} \nLista dos times do Brasileirão: {times22}')
+print(f'{LINHA} \nOs 5 primeiros são {times22[0:5]}')
+print(f'{LINHA} \nOs 4 últimos são {times22[-5:]}')
+print(f'{LINHA} \nTimes em ordem alfabética: {sorted(times22)}')
+print(f'{LINHA} \nO São Paulo está na posção: {times22.index("São Paulo")}\n')
