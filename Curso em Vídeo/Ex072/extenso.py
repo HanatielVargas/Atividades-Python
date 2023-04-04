abae0c160872bed875548c20a121e599e640cbdc
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-04 08:35:55 -0400

    Update extenso.py

--- Curso em Vídeo/Ex072/extenso.py
+++ Curso em Vídeo/Ex072/extenso.py
@@ -5,10 +5,17 @@ Trnasformar um número digitado para escrito por extenso usando tuplas
 num = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 
        'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-val = int(input('Digite um número de 0 a 20: '))
-
-while 0 > val or val > 20:
-    print('\033[1;31mEscreva Certo!\033[m')
-    val = int(input('Digite um número de 0 a 20: '))
+while True:
+    try:
+        val = int(input('Digite um número de 0 a 20: '))
+        if val < 0 or val > 20:
+            raise TypeError
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...')
+        exit()
+    except:
+        print('\033[1;31mEscreva Certo!\033[m')
+    else:
+        break
 
 print(f'Você digitou o número {num[val]}\n')
