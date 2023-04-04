67d00ead3c7b0bb59acf23d4f2a4e3f9830f7fbc
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-04 14:19:04 -0400

    Update times.py

--- Curso em Vídeo/Ex 073/times.py
+++ Curso em Vídeo/Ex 073/times.py
@@ -8,6 +8,6 @@ LINHA = '-='*30
 
 print(f'{LINHA} \nLista dos times do Brasileirão: {times22}')
 print(f'{LINHA} \nOs 5 primeiros são {times22[0:5]}')
-print(f'{LINHA} \nOs 4 últimos são {times22[-5:]}')
+print(f'{LINHA} \nOs 4 últimos são {times22[-4:]}')
 print(f'{LINHA} \nTimes em ordem alfabética: {sorted(times22)}')
-print(f'{LINHA} \nO São Paulo está na posção: {times22.index("São Paulo")}\n')
+print(f'{LINHA} \nO São Paulo está na posção: {times22.index("São Paulo")+1}\n')
