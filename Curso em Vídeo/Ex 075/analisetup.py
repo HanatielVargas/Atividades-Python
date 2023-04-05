571fc60d51bfd87ab0b7783055476818f749f9df
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-05 15:51:31 -0400

    Update analisetup.py

--- Curso em Vídeo/Ex 075/analisetup.py
+++ Curso em Vídeo/Ex 075/analisetup.py
@@ -2,7 +2,51 @@
 Analisador de tuplas
 '''
 
-tup = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite outro número: ')), int(input('Digite outro número: ')))
+while True:
+    try:
+        a = int(input('Digite um número: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        exit()
+    except:
+        print('\033[1;31mOcorreu um erro. Digite certo!\033[m')
+    else:
+        break
+
+while True:
+    try:
+        b = int(input('Digite outro número: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        exit()
+    except:
+        print('\033[1;31mOcorreu um erro. Digite certo!\033[m')
+    else:
+        break
+
+while True:
+    try:
+        c = int(input('Digite outro número: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        exit()
+    except:
+        print('\033[1;31mOcorreu um erro. Digite certo!\033[m')
+    else:
+        break
+
+while True:
+    try:
+        d = int(input('Digite outro número: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        exit()
+    except:
+        print('\033[1;31mOcorreu um erro. Digite certo!\033[m')
+    else:
+        break
+
+tup = (a, b, c, d)
 
 par = 0 
 
@@ -12,5 +56,10 @@ for c in range(len(tup)):
 
 print(f'Você digitou os valores: {tup}')
 print(f'O número 9 apareceu {tup.count(9)} vezes')
-print(f'O número 3 apareceu na {tup.index(3)}ª posição')
-print(f'Os valores pares digitados foram {par}')
+
+try:
+    print(f'O número 3 apareceu na {tup.index(3)}ª posição')
+except:
+    print('O número 3 apareceu não apareceu')
+
+print(f'Os valores pares digitados foram {par}\n')
