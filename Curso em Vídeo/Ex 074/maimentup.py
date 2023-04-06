44c48e211db6ca10b5622ee4267c1ca0a5b52cef
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-05 21:06:54 -0400

    Update maimentup.py

--- Curso em Vídeo/Ex 074/maimentup.py
+++ Curso em Vídeo/Ex 074/maimentup.py
@@ -6,9 +6,6 @@ from random import randint
 
 tup = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
 
-print(f'Os valores sorteados foram: ', end='')
-for c in range(len(tup)):
-    print(tup[c], end=' ')
-
-print(f'\nO maior número sorteado foi {max(tup)}')
+print(f'Os valores sorteados foram: {" ".join(map(str, tup))}')
+print(f'O maior número sorteado foi {max(tup)}')
 print(f'O menor número sorteade foi {min(tup)}\n')
