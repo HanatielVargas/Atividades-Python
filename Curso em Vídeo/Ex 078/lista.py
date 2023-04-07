792cb78846b792386b27116814bd3cd12ca09052
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-07 12:32:25 -0400

    Update lista.py

--- Curso em Vídeo/Ex 078/lista.py
+++ Curso em Vídeo/Ex 078/lista.py
@@ -5,8 +5,17 @@ Maior e Menor valores na Lista
 lista = []
 
 for c in range(5):
-    num = int(input(f'Digite um valor para a posição {c}: '))
-    lista.append(num)
+    while True:
+        try:
+            num = int(input(f'Digite um valor para a posição {c}: '))
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...')
+            exit()
+        except:
+            print('\033[1;31mOcorreu um erro! Digite Novamente.\033[m')
+        else:
+            lista.append(num)
+            break
 
 print(f'Você digitou os valores: {lista}')
 print(f'O maior valor digitado foi {max(lista)} nas posições ', end='')
@@ -14,10 +23,8 @@ print(f'O maior valor digitado foi {max(lista)} nas posições ', end='')
 for c in range(len(lista)):
     if lista[c] == max(lista):
         print(f'{c}... ', end='')
-    
-print('')
 
-print(f'O menor valor digitado foi {min(lista)} nas posições ', end='')
+print(f'\nO menor valor digitado foi {min(lista)} nas posições ', end='')
 
 for c in range(len(lista)):
     if lista[c] == min(lista):
