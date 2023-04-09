9ade751b11f9510a8635434cf3df33e8240cb5ce
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-08 21:49:32 -0400

    Update uniclist.py

--- Curso em Vídeo/Ex 079/uniclist.py
+++ Curso em Vídeo/Ex 079/uniclist.py
@@ -5,14 +5,36 @@ Valores unicos numa lista
 lista = []
 
 while True:
-    num = int(input('Digite um número: '))
+    while True:
+        try:
+            num = int(input('Digite um número: '))
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nSaindo...')
+            exit()
+        except:
+            print('\033[1;31mOcorreu um erro! Escreva certo.\033[m')
+        else:
+            break
 
     lista.append(num)
     print('Número salvo com sucesso...')
 
-    cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while True:
+        try:
+            cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+            if cont not in 'SN':
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nFinalizando...')
+            cont = 'N'
+            break
+        except:
+            print('\033[1;31mOcorreu um erro! Escreva certo.\033[m')
+        else:
+            break
+    
     if cont == 'N':
         break
 
 print('-='*20)
-print(f'Você digitou os valores: {sorted(list(set(lista)))}')
+print(f'Você digitou os valores: {sorted(list(set(lista)))}\n')
