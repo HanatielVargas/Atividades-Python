4a8b8c8461cdd5d5eaeed5732a03922d76b0858f
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-04-13 16:39:15 -0400

    Update listparimp.py

--- Curso em Vídeo/Ex 082/listparimp.py
+++ Curso em Vídeo/Ex 082/listparimp.py
@@ -9,7 +9,16 @@ impar = []
 parar = str
 
 while parar != 'N':
-    num = int(input('Digite um número: '))
+    while True:
+        try:
+            num = int(input('Digite um número: '))
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            exit()
+        except:
+            print('\033[1;31mOcorreu um erro! Digite Novamente...\033[m')
+        else:
+            break
 
     if num % 2 == 0:
         par.append(num)
@@ -18,7 +27,18 @@ while parar != 'N':
     
     lista.append(num)
 
-    parar = str(input('Quer coninuar? [S/N] ')).strip().upper()[0]
+    while True:
+        try:
+            parar = str(input('Quer coninuar? [S/N] ')).strip().upper()[0]
+            if parar not in 'SN':
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            exit()
+        except:
+            print('\033[1;31mOcorreu um erro! Digite Novamente...')
+        else:
+            break
 
 print('-='*20)
 print(f'A lista completa é {lista}')
