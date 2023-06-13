7fb539c971e6739eeb96d38aaeef1b3de31b271b
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-13 19:09:07 -0400

    Update listparimp.py

--- Curso em Vídeo/Ex 085/listparimp.py
+++ Curso em Vídeo/Ex 085/listparimp.py
@@ -6,11 +6,21 @@ listapar = list()
 listaimpar = list()
 
 for c in range(7):
-    num = int(input(f'Digite o {c+1}º número: '))
+    while True:
+        try:
+            num = int(input(f'Digite o {c+1}º número: '))
+        except KeyboardInterrupt:
+            print('Usuário não quer continuar! Encerrando...')
+            exit()
+        except:
+            print('Você digitou algo errado!')
+        else:
+            break
     if num % 2 == 0:
         listapar.append(num)
     else:
         listaimpar.append(num)
 
+print('=-'*30)
 print(f'Os valores pares digitados foram: {listapar}')
-print(f'Os valores pares digitados foram: {listaimpar}')
+print(f'Os valores ímpares digitados foram: {listaimpar}\n')
