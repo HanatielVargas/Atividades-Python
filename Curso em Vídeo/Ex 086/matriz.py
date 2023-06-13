d058c579393c4985cb8b196e461279fd957f4a1e
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-13 19:33:47 -0400

    Update matriz.py

--- Curso em Vídeo/Ex 086/matriz.py
+++ Curso em Vídeo/Ex 086/matriz.py
@@ -6,7 +6,16 @@ matriz = ''
 
 for l in range(3):
     for c in range(3):
-        num = f'{str(input(f"Digite um valor para a posição [{l}][{c}]: ")):^5}'
+        while True:
+            try:
+                num = f'{int(input(f"Digite um valor para a posição [{l}][{c}]: ")):^5}'
+            except KeyboardInterrupt:
+                print('\nUsuário não quer continuar! Encerrando...\n')
+                exit()
+            except:
+                print('Você digitou algo errado!')
+            else:
+                break
         matriz += f'[{num}]'
     matriz += '\n'    
 
