d1e8cc8a5959198baa6955972c366f54856b3e54
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-14 10:50:48 -0400

    Update dict.py

--- Curso em Vídeo/Ex 090/dict.py
+++ Curso em Vídeo/Ex 090/dict.py
@@ -4,10 +4,34 @@ Usando dicionário para guardar nome, média e situação de um aluno
 
 aluno = dict()
 
-nome = str(input('Nome: ')).strip()
-aluno.update({'Nome': nome})
-media = float(input(f'Média de {nome}: '))
-aluno.update({'Média': media})
+while True:
+    try:
+        nome = str(input('Nome: ')).strip()
+        if nome == '' or nome.isalnum() and not nome.isalpha():
+            raise TypeError
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar! Encerrando...\n')
+        exit()
+    except:
+        print('Você digitou algo errado.')
+    else:
+        aluno.update({'Nome': nome})
+        break
+
+while True:
+    try:
+        media = float(input(f'Média de {nome}: '))
+        if 0 > media or media > 10:
+            raise TypeError
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar! Encerrando...\n')
+        exit()
+    except:
+        print('Você digitou algo errado.')
+    else:
+        aluno.update({'Média': media})
+        break
+
 sit = str()
 if media >= 7:
      sit = 'Aprovado'
