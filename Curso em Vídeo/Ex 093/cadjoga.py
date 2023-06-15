b1572cef2eb1a57970f8a05aaf386127ef893c14
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-15 10:27:00 -0400

    Update cadjoga.py

--- Curso em Vídeo/Ex 093/cadjoga.py
+++ Curso em Vídeo/Ex 093/cadjoga.py
@@ -5,12 +5,42 @@ Criando um Cadastro de jogador
 gols = list()
 jogador = dict()
 
-jogador['Nome'] = str(input('Nome do Jogador: ')).strip().title()
-partidas = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+while True:
+    try:
+        jogador['Nome'] = str(input('Nome do Jogador: ')).strip().title()
+        if jogador['Nome'] == '':
+            raise TypeError
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        quit()
+    except:
+        print('Você digitou algo errado!')
+    else:
+        break
+
+while True:
+    try:
+        partidas = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        quit()
+    except:
+        print('Você digitou algo errado!')
+    else:
+        break
 
 for c in range(partidas):
-    gol = int(input(f'\tQuantos gols na partida {c}? '))
-    gols.append(gol)
+    while True:
+        try:
+            gol = int(input(f'\tQuantos gols na partida {c}? '))
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            quit()
+        except:
+            print('Você digitou algo errado!')
+        else:
+            gols.append(gol)
+            break
 
 jogador['Gols'] = gols
 jogador['Total'] = sum(gols)
@@ -28,4 +58,4 @@ print(f'O jogador {jogador["Nome"]} jogou {len(jogador["Gols"])} partidas.')
 for c, i in enumerate(jogador['Gols']):
     print(f'\t=> Na partida {c}, fez {i} gols')
 
-print(f'Foi um total de {len(jogador["Gols"])}')
+print(f'Foi um total de {sum(jogador["Gols"])} gols')
