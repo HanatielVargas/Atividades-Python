fbe7ce35f36e404f2fce1ad9a96c5892ca8d7130
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-16 09:57:47 -0400

    Update dictlist.py

--- Curso em Vídeo/Ex 094/dictlist.py
+++ Curso em Vídeo/Ex 094/dictlist.py
@@ -7,19 +7,63 @@ povo = list()
 soma = 0
 
 while True:
-    pessoa['Nome'] = str(input('Nome: '))
-    pessoa['Sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
-    pessoa['Idade'] = int(input('Idade: '))
+    while True:
+        try:
+            pessoa['Nome'] = str(input('Nome: '))
+            if pessoa['Nome'] == '':
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            quit()
+        except:
+            print('ERRO! Digite algum nome.')
+        else:
+            break
+    
+    while True:
+        try:
+            pessoa['Sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+            if pessoa['Sexo'] not in 'FM':
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            quit()
+        except:
+            print('ERRO! Digite somente M ou F.')
+        else:
+            break
+
+    while True:
+        try:
+            pessoa['Idade'] = int(input('Idade: '))
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            quit()
+        except:
+            print('ERRO! Digite sua idade.')
+        else:
+            break
 
     soma += pessoa['Idade']
 
     povo.append(pessoa.copy())
     pessoa.clear()
 
-    cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while True:
+        try:
+            cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+            if cont not in 'SN':
+                raise TypeError
+        except KeyboardInterrupt:
+            print('\nUsuário não quer continuar!\nEncerrando...\n')
+            quit()
+        except:
+            print('ERRO! Digite somente S ou N.')
+        else:
+            break
+
     if cont == 'N':
         break
-
 
 print('=-'*25)
 print(f'A) Ao todo temos {len(povo)} pessoas cadastradas')
@@ -27,7 +71,7 @@ print(f'B) A média de idade é de {soma/len(povo):.2f}')
 print(f'C) As mulheres cadastradas foram ', end='')
 for c in range(len(povo)):
     if povo[c]['Sexo'] == 'F':
-        print(povo[c]['Nome'], end='')
+        print(povo[c]['Nome'], end=' ')
 
 print(f'\nD) Lista das pessoas que estão acima da média:')
 for c in range(len(povo)):
