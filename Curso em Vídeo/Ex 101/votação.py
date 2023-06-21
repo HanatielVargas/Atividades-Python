5922cb7216395d975b0bc42d2e3a38b065a36591
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-21 14:21:07 -0400

    Update votação.py

--- Curso em Vídeo/Ex 101/votação.py
+++ Curso em Vídeo/Ex 101/votação.py
@@ -2,18 +2,35 @@
 Criando uma função para votação
 '''
 
-from datetime import datetime
-
-
 def vota(nasc):
+    from datetime import datetime
+
     idade = datetime.now().year - nasc
+    if idade < 0:
+        print('Idade menor que 0.')
+        return 0
     print(f'Com {idade} anos: ', end='')
     if idade >= 65 or 16 <= idade < 18:
-        print('Voto OPICIONAL\n')
+        print('Voto OPICIONAL')
     elif 18 <= idade < 65:
-        print('Voto OBRIGATÓRIO\n')
+        print('Voto OBRIGATÓRIO')
     else:
-        print('NÃO VOTA!\n')
+        print('NÃO VOTA!')
+    
+    return 1
 
 
-vota(int(input('Em que ano você nasceu? ')))
+while True:
+    try:
+        print('=-'*20)
+        nascimento = int(input('Em que ano você nasceu? '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        quit()
+    except:
+        print('Você digitou algo errado.')
+    else:
+        vot = vota(nascimento)
+        if vot == 1:
+            print()
+            break
