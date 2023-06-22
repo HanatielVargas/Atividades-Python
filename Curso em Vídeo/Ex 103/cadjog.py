4d38336487583859ea06da51d43d4f3c4e7a00ab
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-22 16:05:51 -0400

    Update cadjog.py

--- Curso em Vídeo/Ex 103/cadjog.py
+++ Curso em Vídeo/Ex 103/cadjog.py
@@ -2,12 +2,34 @@
 Criando uma função para cadastrar jogadores 
 '''
 
-def jogador(nome='<Desconhecido>', gols='0'):
-    if gols not in '0123456789' or gols == '':
-        gols = '0'
-    if nome == '':
-        nome = '<Desconhecido>'
+def jogador(nome='<Desconhecido>', gols=0):
     print(f'O jogador {nome} fez {gols} gol(s) no campeonato!\n')
 
 
-jogador(nome=str(input('Nome do Jogador: ')).strip(), gols=str(input('Número de gols: ')).strip())
+while True:
+    try:
+        nome = str(input('Nome do Jogador: ')).strip()
+        if nome == '':
+            raise TypeError
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        quit()
+    except:
+        nome = '<Desconhecido>'
+        break
+    else:
+        break
+
+while True:
+    try:
+        gols = int(input('Número de gols: '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        quit()
+    except:
+        gols = 0
+        break
+    else:
+        break
+
+jogador(nome, gols)
