ef0074868577bf1aa511ae261360454f26d00393
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-22 16:21:58 -0400

    Update leiaint.py

--- Curso em Vídeo/Ex 104/leiaint.py
+++ Curso em Vídeo/Ex 104/leiaint.py
@@ -4,11 +4,15 @@ Criando uma função para ler números inteiros
 
 def leiaInt():
     while True:
-        num = str(input('Digite um número: ')).strip()
-        if num.isnumeric():
-            return num
-        else:
+        try:
+            num = int(input('Digite um número: '))
+        except KeyboardInterrupt:
+            print('Usuário não quer continuar!\nEncerrando...\n')
+            quit()
+        except:
             print('Você digitou errado. Digite um número inteiro válido!')
+        else:
+            return num
 
 
 num = leiaInt()
