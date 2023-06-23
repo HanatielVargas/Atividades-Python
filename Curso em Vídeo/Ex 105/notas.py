67b7db814f10121a48e50bf1e7e1f8098d5f4f8b
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-23 15:36:38 -0400

    Create notas.py

---
+++ Curso em Vídeo/Ex 105/notas.py
@@ -0,0 +1,31 @@
+'''
+Analisando e Gerando Resultados
+'''
+
+
+def notas(*nums, sit=False):
+    """notas(*nums, sit=False)
+-> Função para analisar notas e situações de vários alunos.
+:param n: uma ou mais notas dos alunos (aceita várias)
+:param sit: valor opcional, indicando se deve ou não adicionar a situação
+:return: dicionário com várias informações sobre a situação da turma.
+"""
+    lista = (nums)
+    dicio = dict()
+    dicio['Total'] = len(lista)
+    dicio['Maior'] = max(lista)
+    dicio['Menor'] = min(lista)
+    dicio['Média'] = sum(lista)/len(lista) # type: ignore
+    if sit:
+        if dicio['Média'] >= 7:
+            dicio['Situação'] = 'Aprovado'
+        elif dicio['Média'] >= 6:
+            dicio['Situação'] = 'Recuperação'
+        else:
+            dicio['Situação'] = 'Reprovado'
+    
+    return dicio
+
+
+resp = notas(5.9, sit=True)
+print(resp)
