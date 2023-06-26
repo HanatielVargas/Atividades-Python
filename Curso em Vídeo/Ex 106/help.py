4c68dc78d708545932320f254102f9c3c923ef35
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-26 16:50:12 -0400

    Update help.py

--- Curso em Vídeo/Ex 106/help.py
+++ Curso em Vídeo/Ex 106/help.py
@@ -2,25 +2,37 @@
 Criando um menu que mostra o help() das coisas
 '''
 
+cores = ['\033[m',      #Normal
+         '\033[0;39;41m',   #Vermelho
+         '\033[0;39;42m',   #Verde
+         '\033[0;39;43m',   #Amarelo
+         '\033[0;39;44m',   #Roxo
+         '\033[0;39;45m',   #Rosa
+         '\033[0;39;46m',   #Azul
+         '\033[0;39;47m',   #Bege
+         '\033[7;30m']      #Branco
+
 def ajuda(msg):
+    titulo(f"Acessando o manual do comando '{func}'", 6)
     help(msg)
 
 
-def msg(txt):
-    txt = '~'*(len(txt)+4)+f'\n  {txt}\n'+'~'*(len(txt)+4)
-    return txt
+def titulo(txt, cor):
+    print(cores[cor], end='')
+    print('~'*(len(txt)+4))
+    print(f'  {txt}')
+    print('~'*(len(txt)+4))
+    print(cores[0], end='')
 
 
 while True:
-    print(msg('SISTEMA DE AJUDA PyHELP'))
+    titulo('SISTEMA DE AJUDA PyHELP', 2)
 
     func = str(input('Função ou Bblioteca > '))
 
     if func.upper() == 'FIM':
-        print(msg('Até Logo!'))
+        titulo('Até Logo!', 7)
         break
-
-    print(msg(f'Acessando o manual do comando \'{func}\''))
 
     ajuda(func)
 
