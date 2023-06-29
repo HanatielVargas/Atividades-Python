cb7c49c7900d09ff635995fed309f329050b562c
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-06-28 21:29:16 -0400

    Update Ex 108

--- Curso em Vídeo/Ex 108/main.py
+++ Curso em Vídeo/Ex 108/main.py
@@ -15,6 +15,20 @@ while True:
     else:
         break
 
+
+while True:
+    try:
+        aumento = float(input('Digite fator de mudança: % '))
+    except KeyboardInterrupt:
+        print('\nUsuário não quer continuar!\nEncerrando...\n')
+        quit()
+    except:
+        print('Você digitou algo errado! Por favor corrija.')
+    else:
+        break
+
+
 print(f'\nA metade de {brl(preco)} é igual a {brl(metade(preco))}')
 print(f'O dobro de {brl(preco)} é igual a {brl(dobro(preco))}')
-print(f'Aumentando 10%, temos {brl(aumentar(preco, 10))}')
+print(f'Aumentando {aumento:.2f}%, temos R${brl(aumentar(preco, aumento))}')
+print(f'Diminuindo {aumento:.2f}%, temos R${brl(diminuir(preco, aumento))}\n')
