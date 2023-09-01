a3b1297667ffca17a5c32fce83508c4e85156830
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2023-09-01 14:32:11 -0400

    Update Ex 115

--- Curso em Vídeo/Ex 115/comandos/__init__.py
+++ Curso em Vídeo/Ex 115/comandos/__init__.py
@@ -9,10 +9,25 @@ def menu():
 \033[33m2\033[m - \033[36mCadastrar novas pessoas\033[m
 \033[33m3\033[m - \033[36mSair do Programa\033[m''')
     print(LINHA)
-    print('\033[33mSua opção: \033[m', end='')
 
 
 def mensagem(msg):
     print(LINHA)
     print(f'{msg:^50}')
     print(LINHA)
+
+
+def cadastrados():
+    dados = 0
+    try:
+        with open('./Curso em Vídeo/Ex 115/dados/dados.txt') as arq:
+            data = arq.readlines()
+            dados = data
+        dados = dados[0].split(';')
+        for c in dados[0:-1]:
+            c = c.split(',')
+            print(f'  {c[0]:<38} {c[1]} Anos')
+    except IndexError:
+        print('\033[1;31mERRO! Não existe pessoa cadastrada.\033[m')
+    except FileNotFoundError:
+        print('\033[1;31mERRO! O arquivo foi apagado.\033[m')
--- Curso em Vídeo/Ex 115/main.py
+++ Curso em Vídeo/Ex 115/main.py
@@ -12,27 +12,27 @@ while not sair:
     menu()
     while True:
         try:
-            opcao = int(input())
+            opcao = int(input('\033[33mSua opção: \033[m'))
             if opcao < 1 or opcao > 3:
                 raise IndexError
         except KeyboardInterrupt:
             print('Usuário não quer continuar!\nEncerrando...')
             quit()
         except:
-            print('ERRO! Digite um valor válido.')
+            print('\033[1;31mERRO! Digite um valor válido.\033[m')
         else:
             break
     
     match opcao:
         case 1:
-            mensagem('Opção 1')
+            mensagem('PESSOAS CADASTRADAS')
+            cadastrados()
         case 2:
-            mensagem('Opção 2')
+            mensagem('FAZER CADASTRO')
         case 3:
+            mensagem('Saindo do sistema... Até logo!')
             sair = True
         case _:
             pass
     
     sleep(1)
-
-mensagem('Saindo do sistema... Até logo!')
